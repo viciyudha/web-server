0ddea7b193e79ef9261a472d0e3797a0f4eab504
Arya Viciyudha <arya.rezavidi@gmail.com>
2017-02-08 23:28:29 +0700

    Initalize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		var d = new Date().toString();
+		console.log('Request: ' + d + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use([middleware.requireAuthentication, middleware.logger]);
+
+app.get('/about', function (req, res) {
+	res.send('About me');
+} );
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log('Server started on port ' + port);
+});
